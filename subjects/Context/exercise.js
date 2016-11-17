a4583377a8cff551b27e61d2f8f72cca805a28de
Andrew Fiedler <andrewkfiedler@gmail.com>
2016-11-17 15:38:46 -0700

    Context - basic solution

--- subjects/Context/exercise.js
+++ subjects/Context/exercise.js
@@ -21,30 +21,56 @@ import React from 'react'
 import { render } from 'react-dom'
 
 class Form extends React.Component {
+  static contextTypes = {
+    handleSubmit: React.PropTypes.func
+  }
   render() {
     return <div>{this.props.children}</div>
   }
 }
 
 class SubmitButton extends React.Component {
+  static contextTypes = {
+    handleSubmit: React.PropTypes.func
+  }
+
   render() {
-    return <button>{this.props.children}</button>
+    return <button onClick={this.context.handleSubmit}>{this.props.children}</button>
   }
 }
 
 class TextInput extends React.Component {
+  static contextTypes = {
+    handleSubmit: React.PropTypes.func
+  }
+
+  onKeyDown = (event) => {
+    if (event.keyCode === 13){
+      this.context.handleSubmit();
+    }
+  }
+
   render() {
     return (
       <input
         type="text"
         name={this.props.name}
         placeholder={this.props.placeholder}
+        onKeyDown={this.onKeyDown}
       />
     )
   }
 }
 
 class App extends React.Component {
+  static childContextTypes = {
+      handleSubmit: React.PropTypes.func
+  }
+
+  getChildContext = () => ({
+    handleSubmit: this.handleSubmit
+  })
+
   handleSubmit = () => {
     alert('YOU WIN!')
   }
@@ -54,7 +80,7 @@ class App extends React.Component {
       <div>
         <h1>This isn't even my final <code>&lt;Form/&gt;</code>!</h1>
 
-        <Form onSubmit={this.handleSubmit}>
+        <Form>
           <p>
             <TextInput name="firstName" placeholder="First Name"/> {' '}
             <TextInput name="lastName" placeholder="Last Name"/>
